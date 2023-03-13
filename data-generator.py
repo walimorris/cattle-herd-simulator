6f6d2eeb24b1aeb1e3704ee8fe891dcb7407685c
Wali Morris <48896622+walimorris@users.noreply.github.com>
2023-03-12 22:59:02 -0700

    Update data-generator.py

--- data-generator.py
+++ data-generator.py
@@ -1,7 +1,7 @@
-import boto3 
 import random
 import math
 import time
+import requests
 
 from decimal import Decimal
 
@@ -67,7 +67,7 @@ def initalizeDeviceLocationData(cattle_ids):
     # generate initial location data for each device in the herd
     # a device is known by the 'cattle_id' each cattle with a device
     for i in range(len(cattle_ids)):
-        coords = offset(47.134559, -106.223248, x, y) # initial coordinates
+        coords = offset(47.134559, -106.223248, x, y)
         cattle_map[cattle_ids[i]] = coords
         y = y + 1
         if (y == 5):
@@ -112,65 +112,70 @@ of cattle moving north and updating a dynamodb table's latitude/longitude data
 attributes every one minutes. Latitude/Longitude values will change every one 
 minute, with a 60 meter north/south offset.
 """
-def herdCattleForOneHour(herding_table, cattle_map):
+def herdCattleForOneHour(cattle_map):
     for i in range(60):
         # After initial location generation, there should be x number of devices with 
         # latitude and longitude data populated in the dynamodb table. Now we should 
         # create a process to update location data every x minutes and update each 
         # device location data in the dynamodb table
         cattle_map = herdCattleForOneMinute(cattle_map)
-        updateDeviceLocationData(herding_table, cattle_map)
+        updateDeviceLocationData(cattle_map)
         print("Location data updated!")
-        
-        
     
 """
 Populate the dynamodb herding table with initial location data for each device
-with a deviceid, latitude attribute and longitude attribute. 
+with a deviceid, latitude attribute and longitude attribute. The DynamoDB Table
+will be updated by sending data attributes through Amazon API Gateway endpoint
+authenticated through a Lambda Authorizer
 
 Iterate through the cattle_map. Each key represents a deviceid and maps to an 
 array that contains two values: [latitude, longitude]
 """
-def populateInitialLocationData(cattle_map, herding_table):
+def populateInitialLocationData(cattle_map):
     # add each device with location to table 
     for cattle_id in cattle_map:
         values = cattle_map[cattle_id]
-        herding_table.put_item(
-            Item = {
-                'deviceid': cattle_id,
-                'latitude': str(values[0]),
-                'longitude': str(values[1])
-            }
-        )
+        postLocationData(str(cattle_id), str(values[0]), str(values[1]), "put")
  
 """
 Update device data in the dynamodb herding table given the deviceid, 
 new latitude and longitude value.
 """
-def updateDeviceLocationData(herding_table, cattle_map):
+def updateDeviceLocationData(cattle_map):
     for cattle_id in cattle_map:
         locationValues = cattle_map[cattle_id]
+        postLocationData(str(cattle_id), str(locationValues[0]), str(locationValues[1]), "update")
         
-        herding_table.update_item(
-            Key = {
-                'deviceid': cattle_id
-            },
-            UpdateExpression = 'SET latitude=:latitude, longitude=:longitude',
-            ExpressionAttributeValues = {
-                ':latitude': str(locationValues[0]),
-                ':longitude': str(locationValues[1])
-            }
-        )
+
+"""
+Sends a POST request with the secret key in the request header to the herding-
+cattle application through the /sendLocations Amazon Gateway Endpoint. The POST
+request contains the cattle-device-id, latitude and longitude data attributes. 
+
+The Authorization parameter in the POST request is used with a Lambda Authorizer. 
+Before API-Gateway routes this request to the integrated Lambda function that will
+process this request, a Lambda Authorizer is initiated that validates the secretcode.
+If the secretcode is valid, Lambda Authorizer will reroute to the API_gateway with 
+authentication access to then initiate the Lambda function the processes the original
+POST request.
+
+update with api-gateway-endpoint and authorization secretcode value
+"""
+def postLocationData(cattle_id, latitude, longitude, saveBehavior): 
+    url = "https://xxxxxxxxxx.execute-api.us-west-2.amazonaws.com?deviceId="+cattle_id+"&latitude="+latitude+"&longitude="+longitude
+    headers = {'Content-Type': 'application/json', 'Authorization': 'secretcode'}
+    r = requests.post(url, headers=headers)
     
+    if (saveBehavior == "put"): 
+        print(r.text + "put: deviceId=" + cattle_id + ", latitude=" + latitude + ", longitude=" + longitude)
+    else: 
+        print(r.text + "update: deviceId=" + cattle_id + ", latitude=" + latitude + ", longitude=" + longitude)
+        
     
-dynamodb = boto3.resource('dynamodb')
 num_cattle_head = 50
 fixed_digits = 6
 low_range = 111111
 high_range = 999999
-
-# dynamodb herding table
-herding_table = dynamodb.Table('your-table-name')
 
 # generate ids for each cattle head 
 cattle_ids = generateCattleIds(num_cattle_head, low_range, high_range, fixed_digits)
@@ -178,7 +183,8 @@ cattle_ids = generateCattleIds(num_cattle_head, low_range, high_range, fixed_dig
 # generate dictionary 
 cattle_map = initalizeDeviceLocationData(cattle_ids)
 
-populateInitialLocationData(cattle_map, herding_table)
+populateInitialLocationData(cattle_map)
 print("Initial Herd data uploaded!")
 
-herdCattleForOneHour(herding_table, cattle_map)
+herdCattleForOneHour(cattle_map)
+print("Herding Sim Complete!")
